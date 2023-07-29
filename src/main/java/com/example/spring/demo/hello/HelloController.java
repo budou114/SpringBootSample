b1c0cd2d7bfa2d4f46d46ee156d7da7a76b08762
budou114 <58349030+budou114@users.noreply.github.com>
2023-07-29 15:40:52 +0900

    first commit

---
+++ src/main/java/com/example/spring/demo/hello/HelloController.java
@@ -0,0 +1,26 @@
+package com.example.spring.demo.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+  @GetMapping("/hello")
+  public String getHello() {
+    // hello.htmlに画面遷移
+    return "hello";
+  }
+
+  @PostMapping("/hello")
+  public String postRequest(@RequestParam("test1") String str, Model model){
+    // 画面から受け取った文字列をModelに登録
+    model.addAttribute("sample", str);
+
+    // respose.htmlに画面遷移
+    return "hello/response";
+  }
+}
